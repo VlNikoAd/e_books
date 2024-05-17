6ea52b78da6a5686cea981df3cffd5ef9a7df391
VlNikoAd <nikolaenko_vladislav@icloud.com>
2024-05-17 23:54:51 +0300

    init

---
+++ src/test/java/org/example/e_books/EBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.e_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
